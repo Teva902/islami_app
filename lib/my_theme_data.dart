5393f3b766f671ebfd380435c4c2c8a7ce8a7ce5
Teva902 <ahmedricrdo510@gmail.com>
2024-07-10 02:16:05 +0300

    Finished quran tab .

--- lib/my_theme_data.dart
+++ lib/my_theme_data.dart
@@ -8,11 +8,16 @@ class MyThemeData {
       appBarTheme: AppBarTheme(
           backgroundColor: Colors.transparent, elevation: 0, centerTitle: true),
       textTheme: TextTheme(
-          bodyLarge: TextStyle(
-        fontSize: 30,
-        fontWeight: FontWeight.bold,
-        color: AppColors.blackColor,
-      )),
-      bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          selectedItemColor: AppColors.blackColor));
+        bodyLarge: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: AppColors.blackColor,
+        ),
+        bodyMedium: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+        bodySmall: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        titleLarge: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+      ),
+      bottomNavigationBarTheme:
+          BottomNavigationBarThemeData(selectedItemColor: AppColors.blackColor),
+      iconTheme: IconThemeData(color: AppColors.blackColor));
 }
