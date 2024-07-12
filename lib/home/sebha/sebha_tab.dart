f8c14179dcb4c418c9b11bec68482c86ae1e98c4
Teva902 <ahmedricrdo510@gmail.com>
2024-07-12 19:12:13 +0300

    Finished hadeth tab and hadeth details . Finish sepha design and radio design .

--- lib/home/sebha/sebha_tab.dart
+++ lib/home/sebha/sebha_tab.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/app_colors.dart';
 
 class SebhaTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Image.asset('assets/images/sebha_head.png'),
+              Image.asset('assets/images/sebha_body.png'),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Text('عدد التسبيحات'),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: 60,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: AppColors.primaryLightColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Center(
+                    child: Text(
+                  '0',
+                  textAlign: TextAlign.center,
+                )),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: 150,
+                height: 60,
+                decoration: BoxDecoration(
+                    color: AppColors.primaryLightColor,
+                    borderRadius: BorderRadius.circular(15)),
+                child: Center(
+                    child: Text(
+                  'سبحان الله',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: AppColors.whiteColor),
+                )),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
