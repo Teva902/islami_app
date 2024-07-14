9ccf5bb98fa08f09f5624da87fc3ffa3bfcc9c9c
Teva902 <ahmedricrdo510@gmail.com>
2024-07-14 09:32:26 +0300

    Finished logic for sepha screen .

--- lib/home/sebha/sebha_tab.dart
+++ lib/home/sebha/sebha_tab.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:islami_app/app_colors.dart';
 
-class SebhaTab extends StatelessWidget {
+class SebhaTab extends StatefulWidget {
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  int clickCount = 0;
+
+  int prayer = 0;
+
+  double rotationAngle = 0.0;
+  String textShow = 'سبحان الله';
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -10,8 +22,20 @@ class SebhaTab extends StatelessWidget {
         Expanded(
           child: Column(
             children: [
-              Image.asset('assets/images/sebha_head.png'),
-              Image.asset('assets/images/sebha_body.png'),
+              Image.asset(
+                'assets/images/sebha_head.png',
+              ),
+              InkWell(
+                  onTap: () {
+                    clickCount++;
+                    TextWillShow();
+                    TextTitle();
+                    rotationAngle += 90;
+                    setState(() {});
+                  },
+                  child: Transform.rotate(
+                      angle: rotationAngle * (3.1415927 / 180),
+                      child: Image.asset('assets/images/sebha_body.png'))),
             ],
           ),
         ),
@@ -28,7 +52,7 @@ class SebhaTab extends StatelessWidget {
                     borderRadius: BorderRadius.circular(15)),
                 child: Center(
                     child: Text(
-                  '0',
+                      '$clickCount',
                   textAlign: TextAlign.center,
                 )),
               ),
@@ -41,7 +65,7 @@ class SebhaTab extends StatelessWidget {
                     borderRadius: BorderRadius.circular(15)),
                 child: Center(
                     child: Text(
-                  'سبحان الله',
+                      textShow,
                   textAlign: TextAlign.center,
                   style: TextStyle(color: AppColors.whiteColor),
                 )),
@@ -51,5 +75,27 @@ class SebhaTab extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  void TextWillShow() {
+    if (clickCount == 31) {
+      clickCount = 0;
+      prayer++;
+    }
+  }
+
+  void TextTitle() {
+    if (prayer == 1) {
+      textShow = 'الحمد لله';
+    } else if (prayer == 2) {
+      textShow = 'استغفر الله';
+    } else if (prayer == 3) {
+      textShow = 'الله اكبر';
+    } else if (prayer == 4) {
+      textShow = 'الملك لله';
+    } else if (prayer >= 5) {
+      prayer = 0;
+      textShow = 'سبحان الله';
+    }
   }
 }
